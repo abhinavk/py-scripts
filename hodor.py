f6e6cb84e5028323b6ddda8611e55d2dac784a3c
Abhinav Kumar <abhinavkumar@outlook.com>
2014-07-20 04:50:57 +0530

    Winter is coming

--- hodor.py
+++ hodor.py
@@ -1,28 +1,30 @@
 # Chat with Hodor
 # Note: Windows has certain problems with this coz Windows!
 
-import hashlib
 import re
 import time
 import random
 
 
 def response(user):
-    '''Formulates a response from Hodor randomly'''
-    md5h = hashlib.md5(user)
-    question_flag = False
-    hodor_speak = '...'
-    if re.search('(?)', user) is not None:
-        question_flag = True
-    return hodor_speak
+    '''Formulates a response from Hodor'''
+    if re.search(r'([wW]inter)', user) or random.randint(0, 100) is 7:
+        return 'Winter is coming. Hodor!'
+    hodor_speak = ['... ', 'Hodor ' * random.randrange(0, 24, 2)]
+    if re.search(r'[\?\!]', user) is not None or random.choice([0, 1]) is 1:
+        hodor_speak.append('huh huh? ')
+    hodor_speak.append('Hodor ' * random.randrange(4, 20, 2))
+    hodor_speak.append('...')
+    return ''.join(hodor_speak)
 
 
 def main():
     '''Main function. Yeah!'''
     print('Hodor is online...\nYou can say \'Hi\'.')
-    user_input = None
+    user_input = ''
     random.seed()   # Seed with system time
     while(str.lower(user_input) != 'bye'):
+        print('You: ', end='')
         user_input = input()
         # \r doesn't work on Windows command line. Neither does ncurses. :(
         print('Hodor is thinking...', end='\r')
@@ -35,7 +37,7 @@ def main():
             time.sleep(user_length/5)
         print('Hodor is typing...  ', end='\r')
         hodor_speak = response(user_input)
-        time.sleep(len(user_input)/5)
+        time.sleep(len(hodor_speak)/50)
         # Clear the last line in case of a short response
         print(' '*20, end='\r')
         print('Hodor says: ' + hodor_speak)
